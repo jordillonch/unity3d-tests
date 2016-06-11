6758cb8a25e5b93b45bf2e168a8100ef24340b6d
Jordi Llonch <llonch.jordi@gmail.com>
2016-06-11 15:40:37 +0200

    first commit

---
+++ Websocket test/Assets/Scripts/WebsocketConnector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+
+public class WebsocketConnector : MonoBehaviour
+{
+  // Use this for initialization
+  void Start()
+  {
+    Debug.Log("log message");
+    using (var ws = new WebSocket("ws://echo.websocket.org"))
+    {
+      ws.OnMessage += (sender, e) =>
+        Debug.Log("Echo says: " + e.Data);
+
+      ws.OnOpen += (sender, args) => { Debug.Log("Websocket connected!"); };
+
+      ws.OnError += (sender, args) =>
+        Debug.Log("Error: " + args.Message.ToString());
+
+      ws.OnClose += (sender, args) =>
+        Debug.Log("Websocket closed because: " + args.Reason.ToString());
+
+      ws.Connect();
+      ws.Send("BALUS");
+      Debug.Log("message sent");
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  }
+}
